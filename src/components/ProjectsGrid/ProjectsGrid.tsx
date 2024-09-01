bc44fd865d49fc24c80aa22cf55c3140a3c42f22
Barış Yaşar <75898912+barisyasar@users.noreply.github.com>
2024-09-01 19:59:54 +0300

    updates

--- src/components/ProjectsGrid/ProjectsGrid.tsx
+++ src/components/ProjectsGrid/ProjectsGrid.tsx
@@ -14,12 +14,12 @@ function ProjectsGrid() {
       >
         <Transition
           variants={{
-            initial: { y: -50, opacity: 0 },
+            initial: { y: 50, opacity: 0 },
             animate: {
               y: 0,
               opacity: 1,
             },
-            exit: { y: -50, opacity: 0 },
+            exit: { y: 50, opacity: 0 },
           }}
           transition={{
             bounce: 0,
